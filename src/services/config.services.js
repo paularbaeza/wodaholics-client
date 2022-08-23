f03de3e18b249cb936c40f35894fed26580982e1
Paula Rodríguez <paularb923@hotmail.com>
2022-08-23 14:46:36 +0100

    listo para deploy

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:5005/api"
+  baseURL: `${process.env.REACT_APP_SERVER_URL}`
 })
 
 // interceptar la llamada y agregar el token
